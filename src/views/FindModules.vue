370014ca5e901a91d05571a4548e9a0e7136f559
matthew-bernardo <matthew@e-bot7.com>
2021-03-21 07:49:58 +0100

    Add arrows to dependency view

--- src/views/FindModules.vue
+++ src/views/FindModules.vue
@@ -41,6 +41,11 @@
             ) Edit
             | {{ module.owner }}
         .empty-layer(v-else) This module is not imported anywhere
+      .up-arrow
+        svg(version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve")
+          g
+            g
+              path(d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068 c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557 l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104 c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z")
       .current
         .module-card.neu.extruded.medium.padding-medium
           .module-card--name.neu.extruded.small {{ dependencyLayers.current.name }}
@@ -53,6 +58,11 @@
               @click.prevent="editModule(dependencyLayers.current)"
             ) Edit
             | {{ dependencyLayers.current.owner }}
+      .up-arrow
+        svg(version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve")
+          g
+            g
+              path(d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068 c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557 l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104 c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z")
       .imports
         .module-card.neu.extruded.small.padding-medium(
           v-for="module in toValues(dependencyLayers.imports)"
@@ -187,6 +197,7 @@ export default {
       flex: 1 1
       overflow: hidden
       display: flex
+      align-items: center
       flex-direction: column
       .empty-layer
         display: flex
@@ -200,6 +211,8 @@ export default {
         flex: 1
         justify-content: center
         overflow-x: auto
+        width: 100%
+        margin-left: 1em
       .current
         flex: 1.2
         align-items: center
@@ -207,6 +220,10 @@ export default {
         height: 180px
         .module-card--value
           overflow: auto
+  .up-arrow
+    height: 32px
+    width: 32px
+    transform: rotate(-90deg)
 </style>
 <style>
 ::-webkit-scrollbar-track {
