9fc8056311008fb6b0501e8e75255b138d63ecc7
matthew-bernardo <matthew@e-bot7.com>
2021-03-21 08:06:50 +0100

    Hacky inline styles for arrow margin

--- src/views/FindModules.vue
+++ src/views/FindModules.vue
@@ -63,7 +63,7 @@
               @click.prevent="editModule(dependencyLayers.current)"
             ) Edit
             | {{ dependencyLayers.current.owner }}
-      .up-arrow
+      .up-arrow(style="margin-bottom: 32px; margin-top: -8px")
         svg(version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve")
           g
             g
